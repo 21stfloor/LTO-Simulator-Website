ac66b64457b287b202d8fea9ac54798cfb780090
21stfloor <21stfloordev@gmail.com>
2024-12-16 22:42:22 +0800

    Update admin.py

--- system/admin.py
+++ system/admin.py
@@ -43,6 +43,22 @@ class TopUpRecordAdmin(admin.ModelAdmin):
     list_display = ('email', 'amount', 'order_id', 'timestamp')
     list_filter = ('timestamp',)
     search_fields = ('email',)
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # If the object exists, make all fields read-only
+            return [field.name for field in self.model._meta.fields]
+        return self.readonly_fields
+
+    # Disable the add functionality
+    def has_add_permission(self, request):
+        return False
+
+    # Disable the change functionality
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    # Disable the delete functionality
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 admin_site.register(TopUpRecord, TopUpRecordAdmin)
